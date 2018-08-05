97a0657d83d4ca3b8280df87b4ed5e76374f280d
BASB-Ichikawa <r.ichikawa@basb.jp>
2018-08-05 20:33:39 +0900

    2018/08/05-2

--- utilities/constants.js
+++ utilities/constants.js
@@ -2,7 +2,8 @@
 exports.CGMODEL_CONTAINER_NAME = 'house3dmodel';
 exports.HOUSE_IMAGE_CONTAINER_NAME = 'houseimage';
 exports.PLAN_IMAGE_CONTAINER_NAME = 'floorimage';
-exports.AUTH_API_URL = 'https://air-mock.azurewebsites.net/air/api/v1/tool/auth/login';
+
+exports.AUTH_API_URL = 'https://air-api.azurewebsites.net/air/api/v1/tool/auth/login';
 
 //exports.DB_URL = 'air-db-server.mysql.database.azure.com';
 //exports.DB_USER = 'airAdmin@air-db-server'
